8007fe0f4b6040daee3b6140cbbf2c4825edd47b
Grey <criro666@gmail.com>
2022-09-09 17:49:27 +0300

    My first js task

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,24 @@
 "use strict";
 
-let str = "ssssss";
+let numberOfFilms,
+    lastFilm,
+    markFilm;
 
-console.log(+str);
+numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "0");
+
+
+const personaMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+for (let i = 0; i < numberOfFilms; i++ ) {
+    lastFilm = prompt("Odin iz filmov?", "");
+    markFilm = prompt ("Na skol`ko ocenite ego?", "");
+    personaMovieDB.movies[lastFilm] = markFilm;
+}
+
+console.log(personaMovieDB);
