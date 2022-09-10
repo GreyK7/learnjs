55b58cfd15c95b1a96dcf6312e5c80483500c4c8
Grey <criro666@gmail.com>
2022-09-10 22:06:47 +0300

    Do few tasks.

--- src/js/script.js
+++ src/js/script.js
@@ -1,24 +1,62 @@
 "use strict";
 
-let numberOfFilms,
-    lastFilm,
-    markFilm;
+// let numberOfFilms,
+//     lastFilm,
+//     markFilm;
 
-numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "0");
+// numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "0");
 
 
-const personaMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
+// const personaMovieDB = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false
+// };
 
-for (let i = 0; i < numberOfFilms; i++ ) {
-    lastFilm = prompt("Odin iz filmov?", "");
-    markFilm = prompt ("Na skol`ko ocenite ego?", "");
-    personaMovieDB.movies[lastFilm] = markFilm;
+// for (let i = 0; i < numberOfFilms; i++ ) {
+//     lastFilm = prompt("Odin iz filmov?", "");
+//     markFilm = prompt ("Na skol`ko ocenite ego?", "");
+//     personaMovieDB.movies[lastFilm] = markFilm;
+// }
+
+// console.log(personaMovieDB);
+
+// for (let i = 0; i < 1; i++) {
+//     let j = 1;
+//     console.log(i);
+// }
+
+
+// console.log(parseInt('111.1', 2));
+
+
+// const arr = [3, 5, 8, 16, 20, 23, 50];
+// const result = [];
+
+// for (let i = 0; i < arr.length; i++) {
+//     result[i] = arr[i];
+// }
+
+// console.log(result);
+
+
+const data = [5, 10, 'Shopping', 20, 'Homework'];
+const result = [];
+
+// for (let i = 0; i < data.length; i++) {
+//     if (isNaN(data[i])) {
+//         data[i] += " - done";
+//     } else {
+//         data[i] *= 2;
+//     }
+// }
+
+// console.log(data);
+
+for (let i = 0; i < data.length; i++) {
+    result[i] = data[data.length - 1 - i];
 }
 
-console.log(personaMovieDB);
+console.log(result);
